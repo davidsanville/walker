f95570692d775cd44f1376332b8c8140eb01a623
aurelius <davids94@comcast.net>
2018-01-26 10:07:10 -0800

    Initial Commit

---
+++ Maze/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Maze
+//
+//  Created by David Sanville on 1/26/18.
+//  Copyright © 2018 David Sanville. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
